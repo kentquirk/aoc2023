09943b1ce08794d662c279fd40e129f16e1f0796
Kent Quirk <kentquirk@honeycomb.io>
2023-12-13 23:03:05 -0500

    Day 13, part 2

--- day13/main.go
+++ day13/main.go
@@ -26,6 +26,7 @@ type block struct {
 	width  int
 	height int
 	data   []int
+	score  int
 }
 
 func loadBlock(s []string) block {
@@ -46,11 +47,19 @@ func loadBlock(s []string) block {
 }
 
 func (b block) String() string {
-	s := ""
+	s := fmt.Sprintf("%d x %d (%d)\n", b.width, b.height, b.score)
 	for _, row := range b.data {
 		s += bstr(row, b.width) + "\n"
 	}
 	return s
+}
+
+func (b block) clone() block {
+	var nb block
+	nb.width = b.width
+	nb.height = b.height
+	nb.data = append([]int(nil), b.data...)
+	return nb
 }
 
 func (b block) column(c int) int {
@@ -61,8 +70,17 @@ func (b block) column(c int) int {
 			n |= (1 << uint(b.height-row-1))
 		}
 	}
-	fmt.Println("Column", c, "is", bstr(n, b.height))
 	return n
+}
+
+func (b block) rotate() block {
+	var nb block
+	nb.width = b.height
+	nb.height = b.width
+	for c := 0; c < b.width; c++ {
+		nb.data = append(nb.data, b.column(c))
+	}
+	return nb
 }
 
 func (b block) checkEqualRows(r1, r2 int) bool {
@@ -85,25 +103,43 @@ func (b block) checkEqualColumns(c1, c2 int) bool {
 	return false
 }
 
-func part1(blocks []string) int {
-	total := 0
-outer:
-	for i, b := range blocks {
-		block := loadBlock(strings.Split(b, "\n"))
-		fmt.Printf("Block %d:\n%s\n", i, block)
-		for r := 0; r < block.height-1; r++ {
-			if block.checkEqualRows(r, r+1) {
-				fmt.Println("Reflection found in block ", i, "at row", r+1)
-				total += 100 * (r + 1)
-				continue outer
+func (b block) findHorizontalReflection(old int) (int, bool) {
+	for r := 0; r < b.height-1; r++ {
+		if b.checkEqualRows(r, r+1) {
+			if r+1 == old {
+				// it's the same one, try again
+				continue
 			}
+			return r + 1, true
 		}
-		for c := 0; c < block.width-1; c++ {
-			if block.checkEqualColumns(c, c+1) {
-				fmt.Println("Reflection found in block ", i, "at col", c+1)
-				total += c + 1
-				continue outer
-			}
+	}
+	return 0, false
+}
+
+func (b block) findVerticalReflection() (int, bool) {
+	for c := 0; c < b.width-1; c++ {
+		if b.checkEqualColumns(c, c+1) {
+			return c + 1, true
+		}
+	}
+	return 0, false
+}
+
+func part1(blocks []block) int {
+	total := 0
+	for i, block := range blocks {
+		// fmt.Printf("Block %d:\n%s\n", i, block)
+		if r, ok := block.findHorizontalReflection(-1); ok {
+			// fmt.Println("Reflection found in block ", i, "at row", r)
+			blocks[i].score = 100 * r
+			total += blocks[i].score
+			continue
+		}
+		if c, ok := block.findVerticalReflection(); ok {
+			// fmt.Println("Reflection found in block ", i, "at col", c)
+			blocks[i].score = c
+			total += blocks[i].score
+			continue
 		}
 		fmt.Println("No reflection found in block ", i)
 	}
@@ -111,9 +147,55 @@ outer:
 	return total
 }
 
+func (b block) findNewHorizontalReflection(old int) (int, bool) {
+	// XOR all pairs of rows together and count bits; if there's 1 bit then we can flip it and
+	// try to see if we have a valid reflection
+	for r1 := 0; r1 < b.height-1; r1++ {
+		for r2 := r1 + 1; r2 < b.height; r2++ {
+			x := b.data[r1] ^ b.data[r2]
+			// fast way to tell if a number is a power of 2 (has only 1 bit set)
+			if (x != 0) && (x&(x-1) == 0) {
+				// We have a pair of rows that differ by 1 bit; try flipping one of them
+				// and see if we have a valid reflection
+				nb := b.clone()
+				nb.data[r1] ^= x
+				if r, ok := nb.findHorizontalReflection(old); ok {
+					// fmt.Printf("FHR\n%s\n", nb)
+					return r, true
+				}
+			}
+		}
+	}
+	return 0, false
+}
+
+func part2(blocks []block) int {
+	total := 0
+	for i, block := range blocks {
+		fmt.Printf("Block %d:\n%s\n", i, block)
+
+		if r, ok := block.findNewHorizontalReflection(block.score / 100); ok {
+			fmt.Println("New reflection found in block ", i, "for score ", r*100)
+			total += 100 * r
+			continue
+		}
+
+		nb := block.rotate()
+		if r, ok := nb.findNewHorizontalReflection(block.score); ok {
+			fmt.Println("New reflection found in block ", i, "for score ", r)
+			total += r
+			continue
+		}
+
+		fmt.Println("No new reflection found in block ", i)
+	}
+
+	return total
+}
+
 func main() {
 	args := os.Args[1:]
-	name := "sample"
+	name := "sample7"
 	if len(args) > 0 {
 		name = args[0]
 	}
@@ -125,6 +207,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	blocks := strings.Split(string(b), "\n\n")
+	sections := strings.Split(string(b), "\n\n")
+	blocks := make([]block, 0)
+	for _, s := range sections {
+		blocks = append(blocks, loadBlock(strings.Split(s, "\n")))
+	}
 	fmt.Printf("Part 1: %d\n", part1(blocks))
+	fmt.Printf("Part 2: %d\n", part2(blocks))
 }
